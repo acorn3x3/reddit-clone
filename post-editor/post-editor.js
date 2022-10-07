6bb09950fa5f7c763e984e4b1d0be41b08a239af
Andrew Cornejo <atcornejo3@gmail.com>
2022-10-07 07:53:48 -0700

    figuring out the form again

--- post-editor/post-editor.js
+++ post-editor/post-editor.js
@@ -1,1 +1,10 @@
 import '../auth/user.js';
+//import
+
+const postForm = document.getElementById('post=form');
+const errorDisplay = document.getElementById('error-display');
+//const imageInput = document.getElementById
+//const preview
+
+/* State*/
+let error = null;
